11f33682c94cf3e0eadd63546e87ebdfa69cb1cf
Philip J Freeman <elektron@halo.nu>
2017-03-20 02:39:07 -0700

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "signalk_client",
-    version = "0.2.1",
+    version = "0.2.2",
     test_suite="tests",
     packages = find_packages(),
     install_requires = [
